9320ce524bffefa8acf8aba0ed46a266b130039b
Vitor Mendonça <vitorm.braga96@gmail.com>
2023-10-03 22:47:27 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -3,33 +3,33 @@ let Xp = 9001
 
 switch (Xp){
   case Xp <= 1000:
-  console.log('O Herói' + nomeHeroi +  ' esta no nivel Ferro')
+  console.log('O Herói ' + nomeHeroi +  ' esta no nivel Ferro')
 break;
 
 case 2001:
-console.log('O Herói' + nomeHeroi +  'esta no nivel bronze')  
+console.log('O Herói ' + nomeHeroi +  ' esta no nivel bronze')  
 break;
   
   case 5001:
-  console.log('O Herói' + nomeHeroi +  'esta no nivel prata')
+  console.log('O Herói ' + nomeHeroi +  ' esta no nivel prata')
   break;
   
   case 7001:
-  console.log('O Herói' +  nomeHeroi  +  'esta no nivel ouro')
+  console.log('O Herói ' +  nomeHeroi  +  ' esta no nivel ouro')
 break;
 
 case 8001:
-  console.log('O Herói' + nomeHeroi +  'esta no nivel platina')
+  console.log('O Herói ' + nomeHeroi +  ' esta no nivel platina')
 break;
 
 case 9001:
-  console.log('O Herói'  +  nomeHeroi  +  'esta no nivel ascendente')
+  console.log('O Herói '   +  nomeHeroi  +   ' esta no nivel ascendente')
   break;
   
   case 10000:
-  console.log('O Herói' + nomeHeroi +  'esta no nivel imortal')
+  console.log('O Herói '  + nomeHeroi +  ' esta no nivel imortal')
   break;
   
   case 10001:
-  console.log('O Herói' + nomeHeroi +  'esta no nivel radiante')
+  console.log('O Herói ' + nomeHeroi +  ' esta no nivel radiante')
  }
