48b1a919f581489c45845bfc4418d7df62e0c1be
Vitor Mendonça <vitorm.braga96@gmail.com>
2023-10-03 22:43:34 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
-let nome = "Rafaella";
-console.log(nome);
+let nomeHeroi = "Vitor"
+let Xp = 9001
+
+switch (Xp){
+  case Xp <= 1000:
+  console.log('O Herói' + nomeHeroi +  ' esta no nivel Ferro')
+break;
+
+case 2001:
+console.log('O Herói' + nomeHeroi +  'esta no nivel bronze')  
+break;
+  
+  case 5001:
+  console.log('O Herói' + nomeHeroi +  'esta no nivel prata')
+  break;
+  
+  case 7001:
+  console.log('O Herói' +  nomeHeroi  +  'esta no nivel ouro')
+break;
+
+case 8001:
+  console.log('O Herói' + nomeHeroi +  'esta no nivel platina')
+break;
+
+case 9001:
+  console.log('O Herói'  +  nomeHeroi  +  'esta no nivel ascendente')
+  break;
+  
+  case 10000:
+  console.log('O Herói' + nomeHeroi +  'esta no nivel imortal')
+  break;
+  
+  case 10001:
+  console.log('O Herói' + nomeHeroi +  'esta no nivel radiante')
+ }
